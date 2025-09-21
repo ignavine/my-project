1eaeda12eff7291d7c91dda8b2d673d2e48169bd
ignavine <ignavine55@gmail.com>
2025-09-20 22:33:50 -1200

    started working on the objects

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ document.getElementById("submit-btn").addEventListener("click", function(){
   const isValid = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/.test(email);
   this.style.borderColor = isValid ? "green" : "red";
 });
+Let=[
+  {tribe:Mankon,Bafut,Mbengwi,Babaki},
+  {Meal:Achu},
+];
+let=[
+{tribes:Kom,Bali,Nso,Wom},
+{Meal:fufu},
+]
+let=[
+{Trbes:Bakossi,Bayang,Balong,Balondo},
+{Meals:Eru},
+]
